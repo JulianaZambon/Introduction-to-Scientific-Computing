7f8f72d3c419f9fa39442b80e068ce5d99129cb8
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2025-03-31 17:41:15 -0300

    Update ZeroFuncao.c

--- EP01/ZeroFuncao.c
+++ EP01/ZeroFuncao.c
@@ -4,27 +4,49 @@
 #include "utils.h"
 #include "ZeroFuncao.h"
 
-// Retorna valor do erro quando método finalizou. Este valor depende de tipoErro
-real_t newtonRaphson (Polinomio p, real_t x0, int criterioParada, int *it, real_t *raiz)
-{
+/*
+ * Critérios de parada:
+ * - Criterio-01: |xk−xk−1|⩽10−6
+ * - Criterio-02: |f(xk)|⩽DBL_EPSILON
+ * - Criterio-03: ULP's entre xk e xk−1⩽2
+ *
+ */
 
+/**
+ * Método de Newton-Raphson para encontrar raízes de um polinômio.
+ *
+ * @param p Polinômio a ser resolvido
+ * @param x0 Chute inicial
+ * @param criterioParada Critério de parada
+ * @param it Número de iterações realizadas
+ * @param raiz Ponteiro para armazenar a raiz encontrada
+ * @return Valor do erro final
+ */
+real_t newtonRaphson(Polinomio p, real_t x0, int criterioParada, int *it, real_t *raiz)
+{
 }
 
-
-// Retorna valor do erro quando método finalizou. Este valor depende de tipoErro
-real_t bisseccao (Polinomio p, real_t a, real_t b, int criterioParada, int *it, real_t *raiz)
+/**
+ * Método da Bissecção para encontrar raízes de um polinômio.
+ *
+ * @param p Polinômio a ser resolvido
+ * @param a Limite inferior do intervalo
+ * @param b Limite superior do intervalo
+ * @param criterioParada Critério de parada
+ * @param it Número de iterações realizadas
+ * @param raiz Ponteiro para armazenar a raiz encontrada
+ * @return Valor do erro final
+ */
+real_t bisseccao(Polinomio p, real_t a, real_t b, int criterioParada, int *it, real_t *raiz)
 {
-
 }
 
-
+/* Cálculo do valor px de um polinômio p */
 void calcPolinomio_rapido(Polinomio p, real_t x, real_t *px, real_t *dpx)
 {
-
 }
 
-
+/* Implementação com o cálculo direto do polinômio e sua derivada, usando a função pow */
 void calcPolinomio_lento(Polinomio p, real_t x, real_t *px, real_t *dpx)
 {
-
 }
