6de19d8bd213209d5b2b4510228abcd35f7f6adc
Juliana Zambon <julianazambon@ufpr.br>
2025-05-15 18:47:43 -0300

    [FIX] script com métricas do LIKWID nao funcionando

--- EP02/ep02_script.sh
+++ EP02/ep02_script.sh
@@ -21,13 +21,18 @@ fi
 # Adiciona uma linha em branco para separar as saídas
 echo ""
 
-# 2. Executa o programa 3 vezes com LIKWID para medir operações FP em dupla precisão
-# obtidos estes valores para o processo de fatoração LU e para o cálculo de cada solução das EDO's
-# LIKWID_MARKER_START("solve_LU")
-# LIKWID_MARKER_STOP("solve_EDO");
-for i in {1..3}; do
-    likwid-perfctr -C 0 -g FLOPS_DP -f -O "$EXEC" 2>&1 \
-    | grep FP_ARITH_INST_RETIRED_SCALAR_DOUBLE \
-    | awk -F',' '{gsub(/ /, "", $3); print $1 "," $3}'
-done
+# 2. Executa o programa com LIKWID para medir operações FP em dupla precisão
+# obtidos estes valores do processo de fatoração LU e do cálculo de cada solução das EDO's
+
+
+
+
+
+
+
+# for i in {1..3}; do
+#     likwid-perfctr -C 0 -g FLOPS_DP -f -O "$EXEC" 2>&1 \
+#     | grep FP_ARITH_INST_RETIRED_SCALAR_DOUBLE \
+#     | awk -F',' '{gsub(/ /, "", $3); print $1 "," $3}'
+# done
 
