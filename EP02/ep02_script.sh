a1d5dc3de3626c35889f4570149ac0a73247f055
JulianaZambon <jz22@inf.ufpr.br>
2025-05-15 21:31:13 -0300

    [FIX] tentando ajeitar o LIKWID

--- EP02/ep02_script.sh
+++ EP02/ep02_script.sh
@@ -21,18 +21,23 @@ fi
 # Adiciona uma linha em branco para separar as saídas
 echo ""
 
-# 2. Executa o programa com LIKWID para medir operações FP em dupla precisão
-# obtidos estes valores do processo de fatoração LU e do cálculo de cada solução das EDO's
+# 2. Para cada conjunto de coeficientes r1 r2 r3 r4, executa o programa com LIKWID no modo marcador (-m)
+#    e extrai a métrica FP_ARITH_INST_RETIRED_SCALAR_DOUBLE das regiões "solve_EDO" e "solve_LU".
+#    Essa métrica representa a quantidade de instruções aritméticas de ponto flutuante em dupla precisão (escalares).
+#    Exemplo de saída:
+#       FP_ARITH_INST_RETIRED_SCALAR_DOUBLE,1517
+#       FP_ARITH_INST_RETIRED_SCALAR_DOUBLE,312
 
-
-
-
-
-
-
-# for i in {1..3}; do
-#     likwid-perfctr -C 0 -g FLOPS_DP -f -O "$EXEC" 2>&1 \
-#     | grep FP_ARITH_INST_RETIRED_SCALAR_DOUBLE \
-#     | awk -F',' '{gsub(/ /, "", $3); print $1 "," $3}'
-# done
-
+likwid-perfctr -C 0 -g FLOPS_DP -m "$EXEC" 2>&1 \
+    | grep 'FP_ARITH_INST_RETIRED_SCALAR_DOUBLE\|Tag solve_EDO\|Tag solve_LU' \
+    | awk -F',' '
+        /Tag solve_EDO/ { in_edo = 1; in_lu = 0; next }
+        /Tag solve_LU/ { in_lu = 1; in_edo = 0; next }
+        /FP_ARITH_INST_RETIRED_SCALAR_DOUBLE/ {
+            if (in_edo) {
+                print "solve_EDO_FP_ARITH_INST_RETIRED_SCALAR_DOUBLE," $3
+            } else if (in_lu) {
+                print "solve_LU_FP_ARITH_INST_RETIRED_SCALAR_DOUBLE," $3
+            }
+        }
+    '
