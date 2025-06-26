a18ad0e65c0456352913c301c40033335b2fce7e
JulianaZambon <jz22@inf.ufpr.br>
2025-06-26 20:43:22 -0300

    add scripts

---
+++ EP03/scripts/gera_graficos.sh
@@ -0,0 +1,19 @@
+# Nome: Juliana Zambon
+# GRR: 20224168
+# Login DINF: jz22
+
+#!/bin/bash
+
+if [ ! -d "graphics" ]; then
+    mkdir graphics
+fi
+
+if [ ! -d "graphics/a" ]; then
+    mkdir graphics/a
+fi
+
+if [ ! -d "graphics/b" ]; then
+    mkdir graphics/b
+fi
+
+python3 create_graphics.py
